228aebee088a65ab976cbc22f869ebd44720c40d
Noel-Telesforo <noeltelesforo9@gmail.com>
2026-06-21 09:03:29 -0600

    feat: change version node

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,16 +2,17 @@ const request = require('supertest');
 const app = require('../src/app');
 
 
-describe('API Test', () => {
-  it('GET / should return message', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.message).toBe('Enterprise SCM Lab');
-  });
+describe("API Test", () => {
+  it("GET / should return message", async () => {
+    const res = await request(app).get("/")
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.message).toBe("Enterprise SCM Lab")
+  })
 
-  it('GET /health should return OK', () => {
-    const res = await request(app).get('/health');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('OK');
-  });
-});
+  it("GET /health should return OK", async () => {
+    const res = await request(app).get("/health")
+    expect(res.statusCode).toBe(200)
+    expect(res.text).toBe("OK")
+  })
+})
+ 
